108135f71ba1c8c1d626bbe6faafc3705d07c873
karanthspradeep <karanthspradeep@gmail.com>
2017-06-24 18:50:21 +0000

    2nd commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, This is my first Project");
+        System.out.println("Hello, This is my first Project. Thanks for looking in to it");
     }
 
 }
