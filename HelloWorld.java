6a5448648a7e305d984230bd11efa433d1789aaa
karanthspradeep <karanthspradeep@gmail.com>
2017-06-24 18:59:38 +0000

    4th commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello Everyone !!! This is my first Java Project.");
-		SystemColor:red
     }
 
 }
